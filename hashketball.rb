1cdad3c412bb562e9b72f39475e30f2e9efe9656
osama miro <o.miro650@gmail.com>
2017-12-22 22:21:17 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -118,9 +118,12 @@ def game_hash
   }
 end
 
+def shoe_size(name)
+  player = find_the_player(name)
+  player.fetch(:shoe)
+end
 
-
-  def num_points_scored(name)
+def num_points_scored(name)
   player = find_the_player(name)
   player.fetch(:points)
 end
